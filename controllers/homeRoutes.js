9be0cf7b04036e3b9a7bb430b37a806760e34a7a
name <nameric@ymail.com>
2022-09-14 20:51:32 -0700

    fixed api files and handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  const userData = await User.findAll({
+    attributes: { exclude: ["password"] },
+  });
+
+  const users = userData.map((project) => project.get({ plain: true }));
+
+  res.render("homepage", {
+    users,
+
+    logged_in: req.session.logged_in,
+  });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+// router.get("/submission", (req, res) => {
+//     if (req.session.logged_in) {
+//       res.redirect("/");
+//       return;
+//     }
+
+// Route "/dashboard"
+
+// Route "/dashboard/new"
+
+// Route "/dashboard/edit/:id"
+
+// Route "/post/:id"
+
+module.exports = router;
